b71a4be3de3c049c3463dceaee0fa7b4335e49b7
SEKI Katei <jiazhen.415@studist.jp>
2025-09-04 14:20:52 +0900

    init minimal app

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>My Vue App</h1>
+        <button @click="fetchMessage">Get Message from Backend</button>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Click the button!'
+        }
+    },
+    methods: {
+        async fetchMessage() {
+            const response = await fetch('http://localhost:8000/api/hello')
+            const data = await response.json()
+            this.message = data.message
+        }
+    }
+}
+</script>
